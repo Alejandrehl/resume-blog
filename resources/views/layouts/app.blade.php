4512dc9a3641660ce8f34ac90abdc98a95047200
Alejandro Exequiel Hernández Lara <alejandrehl@icloud.com>
2019-07-31 08:33:37 -0400

    Seteando header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,21 +3,25 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta name="description" content="Alejandro Hernández Laravel Portfolio Template">
+    <meta name="author" content="Alejandro Hernández">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'RESUME|BLOG') }}</title>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Fonts -->
+    <link rel="icon" href="assets/img/facicon/114x114.png" type="image/gif">
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Quicksand:300,400,500,700" rel="stylesheet">
 
-    <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="assets/vendor/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/vendor/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/vendor/owl.carousel/dist/assets/owl.carousel.min.css">
+    <link rel="stylesheet" href="assets/vendor/linearicons/style.css">
+    <link rel="stylesheet" href="assets/vendor/magnific-popup/dist/magnific-popup.css">
+    <link rel="stylesheet" href="assets/vendor/animate.css/animate.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
     <div id="app">
@@ -76,5 +80,7 @@
             @yield('content')
         </main>
     </div>
+
+    <script src="{{ asset('js/app.js') }}" defer></script>
 </body>
 </html>
